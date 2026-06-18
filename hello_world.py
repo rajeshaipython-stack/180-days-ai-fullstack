be7e84fdd407578e3f2767d8c11f212fa365b461
Rajesh Dhamotharan <rajesh.ai.python@gmail.com>
2026-06-18 21:27:30 +0530

    Day 1: Hello World and variables

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# day 1 - my first python program 
+print("Hello ,world!")
+
+# variables store information so we can use it again later 
+name = "Rajesh"
+goal = " AI FULL STACK DEVELOPER"
+Total_days = 180
+current_day = 1
+
+# now let's use those variables
+print("hi,I'm", name)
+print("my goal:", goal)
+print("Today is day", current_day, "of", Total_days)
