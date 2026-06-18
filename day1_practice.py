44e2511285c5b3f0bf7f3c2a07a07b6e4cf98397
Rajesh Dhamotharan <rajesh.ai.python@gmail.com>
2026-06-18 21:46:10 +0530

    Day 1: practice exercises

---
+++ day1_practice.py
@@ -0,0 +1,11 @@
+# Excercise 1 - variables & sentance
+food = "briyani"
+city = "chennai"
+print("my favourite food is",food, "and i live in", city)
+
+# Excercise 2 - Simple maths with variables 
+a = 10 
+b = 5 
+print("sum", a + b)
+print("difference", a-b)
+print("product", a*b)
