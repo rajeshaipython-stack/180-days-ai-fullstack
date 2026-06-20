1982be14af977235e2e734ebc33f003634ac55b4
Rajesh Dhamotharan <rajesh.ai.python@gmail.com>
2026-06-20 22:21:54 +0530

    Day 3 DSA: list basics (max/min, sum, count)

---
+++ day03_dsa.py
@@ -0,0 +1,12 @@
+#Find max & min
+nums = [4, 9, 2, 7, 1, 8]
+print("max:",max(nums)) 
+print("min:",min(nums))
+
+#Sum of all elements
+nums = [4, 9, 2, 7, 1, 8]
+print("sum:",sum(nums))
+
+# Count occurrences
+nums = [3, 5, 3, 2, 3, 8]
+print("count:",nums.count(5))
